e3bab438fbdfca72ea33ba61b2befe96766eba56
mathias <angulemathias3@gmail.com>
2018-10-25 13:27:57 +0300

    [CHORE #161255348] Add Django-heroku settings

--- authors/settings.py
+++ authors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,3 +158,6 @@ REST_FRAMEWORK = {
         'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

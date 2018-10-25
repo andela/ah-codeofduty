9e72975f73d39a486081ef9e5f79f3838c475368
AnguleMathias <39617195+AnguleMathias@users.noreply.github.com>
2018-10-25 16:02:35 +0300

    #161255348 Setup and deploy base application to Heroku (#3)
    * [CHORE #161255348] Update requirements.txt
    
    * [CHORE #161255348] Add procfile
    
    * [CHORE #161255348] Collectstatic update
    
    * [CHORE #161255348] Collectstatic update
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Update requirements.txt
    
    * [CHORE #161255348] Add Django-heroku settings
    
    * #161255340 Write unit tests for all functionality (#2)
    
    * [ch #161255340]write unit tests for all functionality
    
    * [ch #161255340]update requirements.txt
    
    * [ch #161255340]add docstrings
    
    * [CHORE #161255348] Update requirements.txt
    
    * [CHORE #161255348] Add procfile
    
    * [CHORE #161255348] Collectstatic update
    
    * [CHORE #161255348] Collectstatic update
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Heroku commit
    
    * [CHORE #161255348] Update requirements.txt
    
    * [CHORE #161255348] Add Django-heroku settings

--- authors/settings.py
+++ authors/settings.py
@@ -11,10 +11,20 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -46,6 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -55,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'authors.urls'
 
@@ -145,3 +158,6 @@ REST_FRAMEWORK = {
         'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

eae05f9fbadebf2957f6893dbaa8124ec39deed7
mathias <angulemathias3@gmail.com>
2018-10-29 08:24:25 +0300

    Feature(Swagger Documentation): API Swagger Documentation
    - Update requirements.txt with django-rest-swagger
    - Add schema view to url
    - Add drf swagger to settings
    - Add drf generics CreateAPIView
    
    [Feature #161255350]

--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 
 from .renderers import UserJSONRenderer
 from .serializers import (
@@ -10,7 +10,7 @@ from .serializers import (
 )
 
 
-class RegistrationAPIView(APIView):
+class RegistrationAPIView(CreateAPIView):
     # Allow any user (authenticated or not) to hit this endpoint.
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
@@ -29,7 +29,7 @@ class RegistrationAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class LoginAPIView(APIView):
+class LoginAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = LoginSerializer

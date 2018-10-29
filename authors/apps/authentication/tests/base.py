8f3430bbc0cf94f3426d45e68c530d97a3903f77
mathias <angulemathias3@gmail.com>
2018-10-29 11:23:03 +0300

    Feature(Swagger Documentation): API Swagger Documentation
    - Refactoring authentication tests
    
    [Feature #161255350]

--- authors/apps/authentication/tests/base.py
+++ authors/apps/authentication/tests/base.py
@@ -18,83 +18,65 @@ class BaseTest(TestCase):
         self.SIGN_UP_URL = '/api/users/'
         self.client = APIClient()
         self.user_data = {
-            'user': {
                 'email': 'zawi@gmail.com',
                 'username': 'zawi',
                 'password': 'password1234'
             }
-        }
 
         self.user_email_exists = {
-            'user': {
                 'email': 'zawi@gmail.com',
                 'username': 'zawadi',
                 'password': 'password1234'
             }
-        }
 
         self.user_username_exists = {
-            'user': {
                 'email': 'zawadi@gmail.com',
                 'username': 'zawi',
                 'password': 'password1234'
             }
-        }
 
         self.user_missing_fields = {
-            'user': {
                 'email': '',
                 'username': '',
                 'password': ''
             }
-        }
+
         self.user_missing_username_parameter = {
-            'user': {
                 'email': 'zawi@gmail.com',
                 'username': '',
                 'password': 'password1234'
             }
-        }
 
-        self.user_missing_email_parameter = {
-            'user': {
+        self.user_missing_email_parameter =  {
                 'email': '',
                 'username': 'zawi',
                 'password': 'password1234'
             }
-        }
+
 
         self.user_inputs_short_password = {
-            'user': {
                 'email': 'zawi@gmail.com',
                 'username': 'zawi',
                 'password': 'pass'
             }
-        }
 
         self.user_inputs_invalid_email = {
-            'user': {
                 'email': 'zawi.com',
                 'username': 'zawi',
                 'password': 'pass'
             }
-        }
 
         self.user_wrong_password= {
-            'user': {
                 'email': 'zawi@gmail.com',
                 'username': 'zawi',
                 'password': 'wrong_password'
             }
-        }
 
         self.user_does_not_exist = {
-            'user': {
                 'email': 'zawizawi@gmail.com',
                 'username': 'zawadi',
                 'password': 'password1234'
             }
-        }
   
     def register_user(self):
         """User registration"""

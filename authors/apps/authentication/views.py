0ba768385fe2deb50fb6a205eb7cc798b9ef1814
mathias <angulemathias3@gmail.com>
2018-10-29 11:11:56 +0300

    Feature(Swagger Documentation): API Swagger Documentation
    - Update drf generics CreateAPIView
    
    [Feature #161255350]

--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -17,7 +17,7 @@ class RegistrationAPIView(CreateAPIView):
     serializer_class = RegistrationSerializer
 
     def post(self, request):
-        user = request.data.get('user', {})
+        user = request.data
 
         # The create serializer, validate serializer, save serializer pattern
         # below is common and you will see it a lot throughout this course and
@@ -35,7 +35,7 @@ class LoginAPIView(CreateAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
-        user = request.data.get('user', {})
+        user = request.data
 
         # Notice here that we do not call `serializer.save()` like we did for
         # the registration endpoint. This is because we don't actually have

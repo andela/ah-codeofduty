f565daacf76066e9fe036618e1e6f42a17d8b0c3
KE-C02QF4HNFVH3 <ke-c02qf4hnfvh3@KE-C02QF4HNFVH3s-MacBook-Pro.local>
2018-10-31 12:01:20 +0300

    Feature(Reset Password): Add Reset Password Endpoint dquote> - Update APIView to CreateAPIView dquote> - update requuirements.txt dquote> [Feature #161255355]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,23 +1,13 @@
 from django.urls import path
 
 from .views import (
-<<<<<<< HEAD
-    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, VerifyAPIView, UserForgotPassword, UserResetPassword
-=======
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView, UserForgotPassword, UserResetPassword
->>>>>>> Feauture(Reset Password): Add Reset Password Endpoint
 )
 
 urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view()),
     path('users/login/', LoginAPIView.as_view()),
-<<<<<<< HEAD
-    path('users/verify/<token>/', VerifyAPIView.as_view()),
-    path('users/forgot-password/', UserForgotPassword.as_view(), name='password_reset'),
-    path('users/reset-password/<token>/', UserResetPassword.as_view(), name='password_reset_done'),
-=======
     path('users/forgot-password/', UserForgotPassword.as_view(), name='password_reset'),
     path('users/reset-password/<token>/<email>', UserResetPassword.as_view(), name='password_reset_done'),
->>>>>>> Feauture(Reset Password): Add Reset Password Endpoint
 ]

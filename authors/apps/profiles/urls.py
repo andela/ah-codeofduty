20f88d0588cecbe53b47e195eae774213f8dc3d0
mathias <angulemathias3@gmail.com>
2018-11-05 13:25:31 +0300

    Feature(User Profile):Users should be able to create their profiles
    - View all profiles
    
    [Feature #161255357]

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from authors.apps.profiles.views import ProfileRetrieveUpdateAPIView
+from authors.apps.profiles.views import ProfileRetrieveUpdateAPIView, ListAPIView
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('<username>', ProfileRetrieveUpdateAPIView.as_view(), name='profile'),
+    path('', ListAPIView.as_view(), name='profiles'),
 ]

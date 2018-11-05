98bafba28373f64bd363c6522c483ccfa891214d
mathias <angulemathias3@gmail.com>
2018-11-05 12:58:18 +0300

    Feature(User Profile):Users should be able to create their profiles
    - GET, PUT and PATCH view for profile
    - Add admin.py with credentials
    - Profile relations migrations
    - Update serializer
    
    [Feature #161255357]

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 
+from authors.apps.profiles.views import ProfileRetrieveUpdateAPIView
+
+app_name = 'profiles'
+
+urlpatterns = [
+    path('<username>', ProfileRetrieveUpdateAPIView.as_view(), name='profile'),
+]

1bc21c1bb89015a888ad55110ec47b89a22c668f
mathias <angulemathias3@gmail.com>
2018-11-05 13:25:31 +0300

    Feature(User Profile):Users should be able to create their profiles
    - View all profiles
    
    [Feature #161255357]

--- authors/apps/profiles/views.py
+++ authors/apps/profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from rest_framework.response import Response
@@ -8,7 +9,7 @@ from django.shortcuts import get_object_or_404
 
 from authors.apps.profiles.renderers import ProfileJSONRenderer
 from authors.apps.profiles.serializers import ProfileSerializer
-from authors.apps.profiles.models import User
+from authors.apps.profiles.models import User, Profile
 
 
 class ProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
@@ -45,3 +46,20 @@ class ProfileRetrieveUpdateAPIView(RetrieveUpdateAPIView):
         serializer.update(request.user.profile, serializer_data)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    class ProfileList(ListAPIView):
+        """View all created profiles"""
+        permission_classes = (AllowAny,)
+        queryset = Profile.objects.all()
+        serializer_class = ProfileSerializer
+        pagination_class = LimitOffsetPagination
+
+        def list(self, request):
+            serializer_context = {'request': request}
+            page = self.paginate_queryset(self.queryset)
+            serializer = self.serializer_class(
+                page,
+                context=serializer_context,
+                many=True
+            )
+            return self.get_paginated_response(serializer.data)

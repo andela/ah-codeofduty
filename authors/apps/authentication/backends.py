7144fb848a73dcc631c0fb8820d41ab97d74a921
maxwellgithinji <maxwellgithinji@gmail.com>
2018-11-06 11:50:37 +0300

    bug(JWT Authentication): Fix linting errors - ensure linting errors are fixed and appropriate response is returned [Feature #161255351]

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -2,17 +2,19 @@
 JWT Configuration module
 """
 import jwt
-import json
+
+from datetime import datetime, timedelta
 
 from django.conf import settings
 from django.http import HttpResponse
 
 from rest_framework import authentication, exceptions
-from rest_framework.authentication import get_authorization_header, BaseAuthentication
+from rest_framework.authentication import (
+    get_authorization_header,
+    BaseAuthentication)
 
 from .models import User
 from authors.settings import SECRET_KEY
-from datetime import datetime, timedelta
 
 
 class JWTAuthentication(BaseAuthentication):
@@ -32,7 +34,7 @@ class JWTAuthentication(BaseAuthentication):
         if not auth or auth[0].lower() != b'bearer':
             return None
 
-        # Check if the correct credemtials were passed in 'request' parameter
+        # Check if the correct credentials were passed in 'request' parameter
         if len(auth) == 1:
             msg = 'Invalid token header. No credentials provided.'
             raise exceptions.AuthenticationFailed(msg)
@@ -41,7 +43,7 @@ class JWTAuthentication(BaseAuthentication):
             msg = 'Invalid token header'
             raise exceptions.AuthenticationFailed(msg)
 
-        # Decode token sucessfully, else catch some errors;
+        # Decode token successfully, else catch some errors;
         # errors can be due to expired signature, in decoding
         # token validity or a user not existing
 
@@ -54,7 +56,7 @@ class JWTAuthentication(BaseAuthentication):
             user = User.objects.get(
                 pk=pk
             )
-        # Except cuustom errors while using the token
+        # Except custom errors while using the token
         except Exception as e:
             if e.__class__.__name__ == 'DecodeError':
                 raise exceptions.AuthenticationFailed('cannot decode token')
--- authors/apps/authentication/serializers.py
+++ authors/apps/authentication/serializers.py
@@ -60,8 +60,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
 class LoginSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    email = serializers.CharField(max_length=255)
-    username = serializers.CharField(max_length=255, read_only=True)
+    email = serializers.CharField(max_length=50)
+    username = serializers.CharField(max_length=20, read_only=True)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(read_only=True)
 
@@ -130,7 +130,6 @@ class LoginSerializer(serializers.Serializer):
         # This is the data that is passed to the `create` and `update` methods
         # that we will see later on.
         return {
-            'id': user.pk,
             'email': user.email,
             'username': user.username,
             'token': JWTAuthentication.encode_token(self, user.pk)

0f2cc0c0c7b6a8bde4e2c34f5e396867beac3d5a
KE-C02QF4HNFVH3 <ke-c02qf4hnfvh3@KE-C02QF4HNFVH3s-MacBook-Pro.local>
2018-11-07 09:23:13 +0300

    Feauture(Articles): Add CRUD functions for Articles - User can create an article - User can fetch all articles - User can fetch a single article - User can update their article - User can delete their article - Add tests for all the CRUD functions [Feature #161255358]

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,12 +1,17 @@
 '''articles/serializers'''
 from rest_framework import serializers
-# django.forms.fields.ImageField
+from django.utils.text import slugify
+from authors.apps.authentication.serializers import UserSerializer
+from ..authentication.models import User
+from rest_framework.exceptions import PermissionDenied
 
 from .models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
     title = serializers.CharField(required=True, max_length=100)
     body = serializers.CharField()
+    images = serializers.ListField(child=serializers.CharField(max_length=1000), min_length=None, max_length=None, required=False)
     description = serializers.CharField()
     slug = serializers.CharField(required=False)
     tags = serializers.ListField(child=serializers.CharField(max_length=25), min_length=None, max_length=None, required=False)
@@ -14,11 +19,9 @@ class ArticleSerializer(serializers.ModelSerializer):
     time_created = serializers.SerializerMethodField()
     time_updated = serializers.SerializerMethodField()
 
-    # TODO: handle images
-    
     class Meta:
         model = Article
-        fields = ('title', 'body', 'description', 'slug', 'tags',
+        fields = ('title', 'body', 'images', 'description', 'slug', 'tags',
                   'time_to_read', 'author', 'time_created', 'time_updated')
 
     def get_time_created(self, instance):
@@ -28,7 +31,27 @@ class ArticleSerializer(serializers.ModelSerializer):
         return instance.time_updated.isoformat()
 
     def create(self, validated_data):
+        email = self.context.get('email')
+        user = User.objects.get(email=email)
+        validated_data["author"] = user
+
+        slug = slugify(validated_data["title"])
+        num = 1
+        while Article.objects.filter(slug=slug).exists():
+            slug = slug + "{}".format(num)
+            num += 1
+        validated_data["slug"] = slug
         return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        email = self.context.get('email')
+        if email != instance.author:
+            raise PermissionDenied
+        instance.title = validated_data.get('title', instance.title)
+        instance.body = validated_data.get('body', instance.body)
+        instance.description = validated_data.get('description', instance.description)
+        instance.tags = validated_data.get('tags', instance.tags)
+        instance.time_to_read = validated_data.get('time_to_read', instance.time_to_read)
+        instance.save()
+        return instance
     
-    def  update(self, instance, validated_data):
-        pass

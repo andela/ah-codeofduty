a13c8c2a0e9634d2805aa2c907ef9a0f92f3cb3a
Njeri Ngigi <njery.ngigi@gmail.com>
2018-12-18 09:41:29 +0300

    (Bug #162723150): Fix Filter by Tags - Change pages returned from 10 to 12 [Bug #162723150]

--- authors/settings.py
+++ authors/settings.py
@@ -207,7 +207,7 @@ REST_FRAMEWORK = {
         'authors.apps.authentication.backends.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 12,
 }
 
 # Sendgrid settings
